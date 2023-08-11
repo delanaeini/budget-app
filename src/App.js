fc4b5da117688fc23579f36b00b3394a2dbafe18
Dela Naeini <delaram.naeini@gmail.com>
2023-08-10 17:44:09 -0700

    categories table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { Table, Button } from "reactstrap";
 
-function App() {
+export default function App() {
+  return <CategoriesList />;
+}
+
+function CategoriesList() {
+  const sampleCategories = [
+    {
+      name: "Home",
+      description: "Rent, Utility, Decor",
+      plannedAmount: "$2000",
+      spentAmount: "$2000",
+    },
+    {
+      name: "Food",
+      description: "Grocery, Restaurant",
+      plannedAmount: "$1000",
+      spentAmount: "$1500",
+    },
+    {
+      name: "Transportation",
+      description: "Gas, Maintenance, Parking",
+      plannedAmount: "$700",
+      spentAmount: "$500",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Categories</h1>
+      <Table>
+        <thead>
+          <td>&nbsp;</td>
+          <th>Planned</th>
+          <th colSpan={2}>Spent</th>
+        </thead>
+        {sampleCategories.map((category) => (
+          <Category category={category} />
+        ))}
+      </Table>
+      <Button className="bg-primary">Add a Category...</Button>
+    </>
   );
 }
 
-export default App;
+function Category({ category }) {
+  return (
+    <>
+      <tr>
+        <th>{category.name}</th>
+        <td rowSpan={2}>{category.plannedAmount}</td>
+        <td rowSpan={2}>{category.spentAmount}</td>
+        <td rowSpan={2}>
+          <Button className="bg-primary text-white">+</Button>
+        </td>
+      </tr>
+      <tr>{category.description}</tr>
+    </>
+  );
+}
